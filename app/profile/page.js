65227159dd812cc3e661dc0be1397d7706782a73
Irfansyah <111201911796@mhs.dinus.ac.id>
2024-07-08 20:43:29 +0700

    add login, home, profile page

---
+++ app/profile/page.js
@@ -0,0 +1,9 @@
+import styles from "../page.module.css";
+
+export default function Profile() {
+    return (
+        <main className={styles.main}>
+            <h1>Halaman Profile</h1>
+        </main>
+    );
+}
